7434e116a3ba7277329812e0b0c916cc3de2ab00
Shahram Roohani <sh.roohani@gmail.com>
2025-04-25 18:48:39 +0100

    Integrate sqlite3

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/sroohani/dumb-launcher/cfg"
+	"github.com/sroohani/dumb-launcher/db"
 	"github.com/sroohani/dumb-launcher/tui"
 )
 
 func main() {
-	tui.Launch(displayName, versionString)
 	var cfgParams cfg.Cfg
 	cfg.Read(&cfgParams)
+	db.Load(cfgParams.DbUrl)
+
+	tui.Launch(displayName, versionString)
 }
